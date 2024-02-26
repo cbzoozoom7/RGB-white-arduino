b15125638c1550b4cb3d53e0be653ba1b9a975c3
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2024-02-25 18:37:03 -0600

    Add files via upload

---
+++ RGB_White.ino
@@ -0,0 +1,16 @@
+const int rgbR0OutPin = 3;
+const int rgbG0OutPin = 5;
+const int rgbB0OutPin = 6;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(rgbR0OutPin,OUTPUT);
+pinMode(rgbG0OutPin,OUTPUT);
+pinMode(rgbB0OutPin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(rgbR0OutPin,255);
+analogWrite(rgbG0OutPin,255);
+analogWrite(rgbB0OutPin,255);
+}
